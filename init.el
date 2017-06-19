aa3ca37fc80cca7a03898b0ba392efaf0c635391
J-Robertson <joe.robertson321@gmail.com>
2017-06-19 20:34:20 +0100

    added some stuff to make haskell and org easier

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 (setq package-enable-at-startup nil)
 
