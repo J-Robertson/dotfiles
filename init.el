e441a5a52da515b2c0899f2ff4817f2a1d325c6f
J-Robertson <joe.robertson321@gmail.com>
2017-06-25 14:16:24 +0100

    somehow changed it to make evaluation of org src work

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
    (quote
     ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
  '(org-bullets-bullet-list (quote ("◉")))
- '(package-selected-packages (quote (org-plus-contrib haskel-mode))))
+ '(package-selected-packages (quote (haskel-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
