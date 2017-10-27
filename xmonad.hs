cdcd0e235603ec2da1f4987bd9e5e06cd1ebf2d8
J-Robertson <joe.robertson321@gmail.com>
2017-10-27 23:32:16 +0100

    overall cleanup

--- xmonad.hs
+++ xmonad.hs
@@ -16,6 +16,7 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Cursor
+import XMonad.Util.EZConfig
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Renamed
 import XMonad.Layout.NoFrillsDecoration
@@ -181,7 +182,6 @@ myTabTheme = def
   , decoHeight          = 15
   }
 
-
 myLayout = mainLayout ||| noBar ||| Full
   where
     mainLayout = named "Tabbed/Tall"
@@ -190,14 +190,14 @@ myLayout = mainLayout ||| noBar ||| Full
                  $ addTabs shrinkText myTabTheme
                  $ subLayout [] (Simplest ||| Accordion)
                  $ spacing 4
-                 $ ResizableTall nmaster delta ratio []
+                 $ Tall nmaster delta ratio
 
     noBar      = named "noBar Tabbed/Tall"
                  $ windowNavigation
                  $ addTabs shrinkText myTabTheme
                  $ subLayout [] (Simplest ||| Accordion)
                  $ spacing 4
-                 $ ResizableTall nmaster delta ratio []
+                 $ Tall nmaster delta ratio
 
     nmaster = 1
     ratio   = 1/2
@@ -308,62 +308,3 @@ myConfig = def {
         logHook            = myLogHook,
         startupHook        = myStartupHook
     }
-
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help = unlines ["The modifier key is 'super'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch gnome-terminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "mod-i  Push the borders up",
-    "mod-o  Push the borders down",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging",
-    "",
-    "-- Xmobar",
-    "mod-b  Toggle appearence of statusbar",
-    "",
-    "-- lockscreen",
-    "mod-Shift-z  do lockscreen",
-    "",
-    "-- Start emacs",
-    "mod-e Start emacs"]
