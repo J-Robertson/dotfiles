607cf429655c1505b8764bbf1cfeb06b859ef6dd
J-Robertson <joe.robertson321@gmail.com>
2017-11-12 18:03:42 +0000

    clean up

--- xmonad.hs
+++ xmonad.hs
@@ -1,17 +1,6 @@
---
--- xmonad example config file.
---
--- A template showing all available configuration hooks,
--- and how to override the defaults in your own xmonad.hs conf file.
---
--- Normally, you'd only override those defaults you care about.
---
-
 import XMonad
 import XMonad.Hooks.DynamicLog
 import System.Exit
-import XMonad.Layout.ResizableTile
-
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 import XMonad.Hooks.ManageHelpers
@@ -28,55 +17,29 @@ import XMonad.Actions.Navigation2D
 import XMonad.Layout.Spacing
 import Data.Monoid (All)
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
---
 myTerminal :: String
 myTerminal      = "xterm"
 
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
--- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
--- Width of the window border in pixels.
---
 myBorderWidth :: Dimension
 myBorderWidth   = 0
 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
 myModMask :: KeyMask
 myModMask       = mod4Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
 myWorkspaces :: [String]
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
--- Border colors for unfocused and focused windows, respectively.
---
 myNormalBorderColor :: String
 myFocusedBorderColor :: String
 myNormalBorderColor  = "#555555"
 myFocusedBorderColor = "#dddddd"
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys = \conf -> mkKeymap conf $
   [("M-S-<Return>", spawn $ terminal conf)
@@ -100,6 +63,7 @@ myKeys = \conf -> mkKeymap conf $
   ,("M-C-k", sendMessage $ pullGroup U)
   ,("M-C-h", sendMessage $ pullGroup L)
   ,("M-C-l", sendMessage $ pullGroup R)
+  ,("M-C-u", withFocused (sendMessage . UnMerge))
   ,("M-=", incSpacing 1)
   ,("M--", incSpacing (-1))
   ,("M-0", setSpacing 0)
@@ -111,55 +75,15 @@ myKeys = \conf -> mkKeymap conf $
   ,("M-S-q", io exitSuccess)
   ,("M-q", spawn "xmonad --recompile; xmonad --restart")
   ,("M-S-z", spawn "slock")
-  ,("M-e", spawn "emacs")
-  ,("M-y", fun)]
-
+  ,("M-e", spawn "emacs")]
   ++
-
   [("M" ++ mask ++ tag, windows $ f tag)
   | tag <- myWorkspaces, (f,mask) <- [(W.greedyView, "-"), (W.shift, "-S-")]]
 
-
-
-
-
-fun :: X ()
-fun = do
-  XState {windowset = old} <- get
-  spawn ("echo \"" ++ (show (W.stack (W.workspace (W.current old)))) ++ "\" > test.out")
-
-
-------------------------------------------------------------------------
--- Mouse bindings: default actions bound to mouse events
---
 myMouseBindings :: XConfig t -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings XConfig {XMonad.modMask = modm} = M.fromList
-
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster)
-
-    -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster)
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
-    ]
-
-------------------------------------------------------------------------
--- Layouts:
-
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
+  [((modm, button1), \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)
+    ,((modm, button3), \w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)]
 
 myTopBarTheme :: Theme
 myTopBarTheme = def
@@ -204,22 +128,6 @@ myLayout = mainLayout ||| noBar ||| Full
     delta   = 3/100
     named n = renamed [XMonad.Layout.Renamed.Replace n]
 
-
-------------------------------------------------------------------------
--- Window rules:
-
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
 myManageHook :: ManageHook
 myManageHook = mconcat
     [ className =? "Gimp"           --> doFloat
@@ -227,50 +135,26 @@ myManageHook = mconcat
     , isDialog                      --> doFloat
     , isFullscreen                  --> doFloat]
 
-
-------------------------------------------------------------------------
--- Event handling
-
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
 myEventHook :: Event -> X All
 myEventHook = mempty
 
-------------------------------------------------------------------------
--- Status bars and logging
-
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
---
 myLogHook :: X ()
 myLogHook = return ()
 
-------------------------------------------------------------------------
--- Startup hook
-
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
 myStartupHook :: X ()
 myStartupHook = do
   spawn "xset -dpms; xset s off"
   spawn "feh --bg-scale /usr/share/backgrounds/gnome/Dark_Ivy.jpg"
+  spawn "xset r rate 300 40"
   setDefaultCursor xC_left_ptr
 
-------------------------------------------------------------------------
--- Run xmonad with xmobar and with a command to stop screen turn off
 myNavigation2DConfig :: Navigation2DConfig
 myNavigation2DConfig = def { unmappedWindowRect = [("Full", singleWindowRect)]
                            , defaultTiledNavigation = centerNavigation}
 
 main :: IO ()
-main = statusBar myBar myPP toggleStrutsKey myConfig >>= xmonad . withNavigation2DConfig myNavigation2DConfig
+main = statusBar myBar myPP toggleStrutsKey myConfig
+       >>= xmonad . withNavigation2DConfig myNavigation2DConfig
 
 myBar :: String
 myBar = "xmobar"
@@ -285,12 +169,7 @@ myPP = def
       , ppTitle   = xmobarColor "#ec7373" "" . shorten 80
       , ppSep     = " | "
       }
--- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will
--- use the defaults defined in xmonad/XMonad/Config.hs
---
--- No need to modify this.
---
+
 myConfig = def {
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
