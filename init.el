b9b7745da4f4a2b313febac5abe261246b399129
Joe Robertson <joe.robertson321@gmail.com>
2018-01-20 12:55:20 +0000

    fixed org mode

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
 (setq package-enable-at-startup nil)
 
