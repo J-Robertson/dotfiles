e5813514b9b89842b597f3adf41a5d6171799925
Joe Robertson <joe.robertson321@gmail.com>
2018-05-31 21:56:01 +0100

    removed a now depricated line

--- init.el
+++ init.el
@@ -2,7 +2,6 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")))
-(package-initialize)
 (setq package-enable-at-startup nil)
 
 (mapcar (lambda (x) (unless (package-installed-p x)
